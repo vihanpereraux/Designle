976390b5b58d47bd2b20f657121d9535884bfe10
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-04-10 11:18:57 +0530

    color space changed

--- Backend/test.py
+++ Backend/test.py
@@ -1,22 +1,25 @@
+# import imp
 import cv2
 from cv2 import imshow
-from colormath.color_objects import AdobeRGBColor, LabColor
+from colormath.color_objects import sRGBColor, LabColor
 from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie2000
+import math
 
 
-red_color = AdobeRGBColor(225, 0, 0) # red color
-lightblue_color = AdobeRGBColor(35, 195, 225) # light-blue color
-pink_color = AdobeRGBColor(238, 0, 255) # pink color
+red_color = sRGBColor(255, 0, 0) # red color -> a channel 80
+orange_color = sRGBColor(255, 142, 225) # orange color -> a channel 67
+purple_color = sRGBColor(193, 142, 219) # purple color -> a channel 53
 
 # Convert from RGB to Lab Color Space
-color1_lab = convert_color(red_color, LabColor)
-color2_lab = convert_color(pink_color, LabColor)
+color1_lab = convert_color(purple_color, LabColor)
+# color2_lab = convert_color(pink_color, LabColor)
 
-print("-----------------------------------------------------------")
-print("a channel of the red color " , color1_lab.lab_a)
-print("a channel of the pink color" , color2_lab.lab_a)
-print("-----------------------------------------------------------")
+if color1_lab.lab_a > 0 :
+    print("-----------------------------------------------------------")
+    print("a channel of the red color " , math.sqrt(color1_lab.lab_a))
+    # print("a channel of the pink color" , color2_lab.lab_a)
+    print("-----------------------------------------------------------")
 
 # delta_e = delta_e_cie2000(color1_lab, color2_lab);
 # print(delta_e)

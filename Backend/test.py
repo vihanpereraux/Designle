f4d2792d5d5011b1931bf9e4790be87282c2aa20
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-07-02 19:14:05 +0530

    color ranges updated and grey scheme added

--- Backend/test.py
+++ Backend/test.py
@@ -10,9 +10,21 @@ import math
 # from sklearn.metrics import check_scoring
 
 
+# checking colors in sceheme 01
 red_color = sRGBColor(255, 0, 0) 
 orange_color = sRGBColor(255, 142, 0) 
 purple_color = sRGBColor(193, 0, 219) 
+
+# checking colors in sceheme 02
+yellow_color = sRGBColor(255, 252, 0) 
+green_color = sRGBColor(12, 207, 0)
+blue_color = sRGBColor(0, 0, 255)
+
+# checking colors in sceheme 03
+black_color = sRGBColor(0, 0, 0)
+dark_grey_color = sRGBColor(61, 61, 61)
+grey_color = sRGBColor(194, 184, 191)
+
 
 def color_section_01(color) :
     color1_lab = convert_color(color, LabColor) # Convert from RGB to Lab Color Space
@@ -26,15 +38,9 @@ def color_section_01(color) :
         print("b channel of the red color " , math.sqrt( (color1_lab.lab_b*-1) )*-1 )
     else :
         print("b channel of the red color " , math.sqrt(color1_lab.lab_b))
+        print("l channel of the red color " , math.sqrt(color1_lab.lab_l))
         print("-----------------------------------------------------------")
 
-# color_section_01(red_color)
-
-
-yellow_color = sRGBColor(255, 252, 0) 
-green_color = sRGBColor(12, 207, 0)
-blue_color = sRGBColor(0, 0, 255) 
-checking_color = sRGBColor(40, 157, 204)
 
 def color_section_02(color) :
     color1_lab = convert_color(color, LabColor) # Convert from RGB to Lab Color Space
@@ -49,7 +55,6 @@ def color_section_02(color) :
     else :
         print("b channel of the red color " , math.sqrt(color1_lab.lab_b))
 
-# color_section_02(green_color)
 
 
 def color_sections(extracted_colors) :
@@ -58,8 +63,12 @@ def color_sections(extracted_colors) :
         sRGB_versions.append(sRGBColor(color[0], color[1], color[2]))
 
     color_1_LAB = convert_color(sRGB_versions[0], LabColor)
-    color_2_LAB = convert_color(sRGB_versions[1], LabColor)
+    # color_2_LAB = convert_color(sRGB_versions[1], LabColor)
 
+    # print("a", math.sqrt(color_1_LAB.lab_a))
+    # print("b", math.sqrt(color_1_LAB.lab_b))
+
+   
     if color_1_LAB.lab_a < 0 :
         color_1_LAB_achannel = int(round(math.sqrt(color_1_LAB.lab_a * -1), 0) * -1)
     else :
@@ -67,6 +76,19 @@ def color_sections(extracted_colors) :
 
     if color_1_LAB.lab_b < 0 :
         color_1_LAB_bchannel = int(round(math.sqrt(color_1_LAB.lab_b * -1), 0) * -1)
+    else :
+        color_1_LAB_bchannel = int(round(math.sqrt(color_1_LAB.lab_b), 0))
+
+    
+    if color_1_LAB.lab_a < 0 :
+        color_1_LAB_achannel = int(round(math.sqrt(color_1_LAB.lab_a * -1), 0) * -1)
+        print("a", color_1_LAB_achannel)
+    else :
+        color_1_LAB_achannel = int(round(math.sqrt(color_1_LAB.lab_a), 0))
+        
+    if color_1_LAB.lab_b < 0 :
+        color_1_LAB_bchannel = int(round(math.sqrt(color_1_LAB.lab_b * -1), 0) * -1)
+        print("b", color_1_LAB_bchannel)
     else :
         color_1_LAB_bchannel = int(round(math.sqrt(color_1_LAB.lab_b), 0))
 
@@ -91,25 +113,30 @@ def color_sections(extracted_colors) :
     # color sceheme 02
     if 60 <= color_1_LAB_bchannel <= 100 and (-40 <= color_1_LAB_achannel <= 0 or 0 <= color_1_LAB_achannel <= 40) : 
         print("Yellow !")
-    if 0 <= color_1_LAB_bchannel <= 60 and (-40 <= color_1_LAB_achannel <= 0 or 0 <= color_1_LAB_achannel <= 40) : 
+    if 30 <= color_1_LAB_bchannel <= 60 and (-40 <= color_1_LAB_achannel <= 0 or 0 <= color_1_LAB_achannel <= 40) : 
         print("Yellow shades !")
 
-    if 0 <= color_1_LAB_bchannel <= 100 and -100 <= color_1_LAB_achannel <= -50 : 
+    if 30 <= color_1_LAB_bchannel <= 100 and -100 <= color_1_LAB_achannel <= -30 : 
         print("Green !")
-    if 0 <= color_1_LAB_bchannel <= 100 and -50 <= color_1_LAB_achannel <= 0 : 
+    if 30 <= color_1_LAB_bchannel <= 100 and -50 <= color_1_LAB_achannel <= -30 : 
         print("Green shades !")
 
     if -100 <= color_1_LAB_bchannel <= -50 and 60 <= color_1_LAB_achannel <= 100 : 
         print("Blue !")
-    if -50 <= color_1_LAB_bchannel <= 0 and (0 <= color_1_LAB_achannel <= 60 or -60 <= color_1_LAB_achannel <= 0) : 
+    if -50 <= color_1_LAB_bchannel <= -30 and (0 <= color_1_LAB_achannel <= 60 or -60 <= color_1_LAB_achannel <= 0) : 
         print("Blue shades !")
     if -100 <= color_1_LAB_bchannel <= -50 and (0 <= color_1_LAB_achannel <= 60 or -60 <= color_1_LAB_achannel <= 0) : 
         print("Blue shades !")
-    
-    print(int(round(math.sqrt(color_1_LAB.lab_l), 0)))
 
-extracted_colors = [ (131, 188, 118), (12, 207, 0) ]
-color_sections(extracted_colors) 
+    # color sceheme 03
+    if -25 <= color_1_LAB_bchannel <= 25 and -25 <= color_1_LAB_achannel <= 25 :
+        print("Grey shades !")
+
+    # print(int(round(math.sqrt(color_1_LAB.lab_l), 0)))
+
+# extracted_colors = [ (131, 188, 118), (12, 207, 0) ]
+# color_section_01(red_color)
+color_sections( [(32, 32, 32)] ) 
 
 
 

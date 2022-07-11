ab5ab78172856cf8777a1048ce9a6cb37bb7e938
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-07-11 08:19:37 +0530

    TfidfVectorizer added

--- designle-full-stack/ux_suggestions.py
+++ designle-full-stack/ux_suggestions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
 import sys
 
